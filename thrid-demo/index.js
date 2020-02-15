b85a853de130960b61f6e749a7b735b190501441
lumeiling <1520622848@qq.com>
2020-02-15 14:02:02 +0800

    first commit(basic Vue)

---
+++ thrid-demo/index.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        todos: [
+            {text: 'Learn Javascript'},
+            {text: 'Learn Vue'},
+            {text: 'Build something awesome'}
+        ]
+    }
+})
