0c2abee3f9fa1a7aaea904fbeb6e5eb989fdcf1c
lumeiling <1520622848@qq.com>
2020-02-16 12:41:23 +0800

    Vue project demo

---
+++ component-bluebutton/BlueButton.vue
@@ -0,0 +1,21 @@
+<template>
+    <button>{{title}}</button>
+</template>
+
+<script>
+    module.exports = {
+        data: function() {
+         return {
+             title: "BlueButton"
+         }
+        }
+    }
+</script>
+
+<style scoped>
+    button {
+        background: blue;
+        color: white;
+        padding: 1rem;
+    }
+</style>
---
+++ component-bluebutton/index.js
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+import BlueButton from './BlueButton'
+
+Vue.component('bluebutton', BlueButton);
+
+new Vue({
+    el: '#app',
+    data: {
+        message: 'Using Single File Components'
+    }
+})
